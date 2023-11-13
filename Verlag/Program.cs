d755a854bba0942da997144da690c9d6d8e7f044
Ertuerk Dogan <ertuerk.d2@gso.schule.koeln>
2023-11-13 10:56:44 +0100

    Buch Klasse mit Konstruktor wurde erstellt angepasst auf die Unittests

--- Verlag/Program.cs
+++ Verlag/Program.cs
@@ -1,2 +1,68 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Verlag
+{
+    public class Buch
+    {
+        private string autor = "";
+        private string titel = "";
+        private int auflage = 0;
+
+        public Buch(string autor, string titel)
+        {
+            this.autor = autor;
+            this.titel = titel;
+        }
+        public Buch(string autor, string titel, int auflage)
+        {
+            if (auflage <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Verlag darf nicht kleiner oder gleich Null sein");
+            }
+            else
+            {
+                this.auflage = auflage;
+            }
+            this.autor = autor;
+            this.titel = titel;
+        }
+
+        public string Autor
+        {
+            get
+            {
+                return autor;
+            }
+            set
+            {
+                autor = value;
+            }
+        }
+        public int Auflage
+        {
+            get
+            {
+                return auflage;
+            }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("Verlag darf nicht kleiner oder gleich Null sein");
+                }
+                else
+                {
+                    auflage = value;
+                }
+            }
+        }
+        public string Titel
+        {
+            get
+            {
+                return titel;
+            }
+        }
+
+    }
+}
